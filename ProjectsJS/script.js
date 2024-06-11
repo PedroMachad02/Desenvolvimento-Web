8a5bd5437ac4a7facd813fcba4a49f7855a0bcf1
PedroMachad02 <pedroamcrc@gmail.com>
2024-06-11 15:20:09 -0300

    Projetos Js

---
+++ ProjectsJS/script.js
@@ -0,0 +1,14 @@
+var hora = new Date().getHours()
+var saudacao
+
+if(hora<12){
+    saudacao = "Bom dia"
+
+}else if(12<hora<18){
+    saudacao = "Boa tarde"
+
+}else{
+    saudacao = "Boa noite"
+}
+
+document.getElementById("mensagem").innerHTML = saudacao
